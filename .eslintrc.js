a99f37e9b7ac01be7398915e953b71e596691b35
Edwin de los santos <edwin.delossantos@bairesdev.com>
2022-01-30 12:18:04 -0400

    eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,60 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': [
+    'plugin:react/recommended',
+    'google',
+  ],
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true,
+    },
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'react',
+  ],
+  'rules': {
+    'array-callback-return': [
+      'error',
+      {
+        allowImplicit: true,
+      },
+    ],
+    'brace-style': 'error',
+    'camelcase': [
+      'error',
+      {
+        'ignoreDestructuring': true,
+        'ignoreImports': true,
+        'allow': ['^UNSAFE_'],
+      },
+    ],
+    'complexity': 'error',
+    'default-case': 'error',
+    'dot-notation': 'error',
+    'grouped-accessor-pairs': 'error',
+    'no-empty-function': 'error',
+    'no-implicit-globals': 'error',
+    'no-magic-numbers': [
+      'error',
+      {ignore: [0, 1, 2], ignoreArrayIndexes: true},
+    ],
+    'no-nested-ternary': 'error',
+    'no-shadow': 'error',
+    'no-var': 'error',
+    'prefer-const': [
+      'error',
+      {
+        destructuring: 'any',
+        ignoreReadBeforeAssign: false,
+      },
+    ],
+    'prefer-promise-reject-errors': 'error',
+    'react/display-name': 'off',
+    'sort-imports': 'error',
+  },
+};
